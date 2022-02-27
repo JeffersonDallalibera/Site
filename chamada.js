53f27e8918c689eb583f96696d7572c0fd7d75fa
Jefferson Dallalibera <jeffersonmigueldallalibera@gmail.com>
2022-02-27 14:52:56 -0300

    first commit

---
+++ chamada.js
@@ -0,0 +1,9 @@
+﻿function chamafoto(){
+    console.log("chamou a função")
+    var img = document.createElement("IMG");
+    img.src = "./img/sophi.jpg";
+    img.style.alignContent ="center";
+    img.style.width = "450";
+    img.style.height = "450px";
+    document.getElementById('imagens').appendChild(img);
+}
